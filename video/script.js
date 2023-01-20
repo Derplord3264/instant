512ea6a0f74c10697e2c15474784b0ef3c5541a7
CaenKoleJones <117491943+CaenKoleJones@users.noreply.github.com>
2023-01-20 12:59:42 -0500

    Update script.js

--- video/script.js
+++ video/script.js
@@ -1,119 +1,187 @@
-// Generate random room name if needed
-if (!location.hash) {
-  location.hash = Math.floor(Math.random() * 0xFFFFFF).toString(16);
-}
-const roomHash = location.hash.substring(1);
+let room, peerConnection;
+let roomName = "observable-" + prompt("Enter room name", "defaultRoom");
+let userName = prompt("Hey there, what's your name?", "Incognito") || "no_name";
 
-// TODO: Replace with your own channel ID
-const drone = new ScaleDrone('5gYhBgieIfaOUv1M');
-// Room name needs to be prefixed with 'observable-'
-const roomName = 'observable-' + roomHash;
-const configuration = {
-  iceServers: [{
-    urls: 'stun:stun.l.google.com:19302'
-  }]
-};
-let room;
-let pc;
-
-
-function onSuccess() {};
-function onError(error) {
-  console.error(error);
-};
-
-drone.on('open', error => {
-  if (error) {
-    return console.error(error);
+// One instance of Scaledrone establishes a single connection, takes parameter 'CHANNEL_ID_FROM_DASHBOARD'.
+let drone = new ScaleDrone("5gYhBgieIfaOUv1M", {
+  data: {
+    name: userName
   }
+});
+
+const configuration = {
+  iceServers: [
+    {
+      urls: "stun:stun.l.google.com:19302"
+    }
+  ]
+};
+
+drone.on("error", error => {
+  console.log(error);
+});
+
+// 'reconnect' event indicates reconnection occured succesfully.
+drone.on("reconnect", () => {
+  console.log("reconnected");
+});
+
+// 'open' event indicates a connection has been opened.
+drone.on("open", error => {
+  if (error) return console.error(error);
+
+  // Subscribe to the room specified by the user.
   room = drone.subscribe(roomName);
-  room.on('open', error => {
+  room.on("open", error => {
     if (error) {
       onError(error);
     }
   });
-  // We're connected to the room and received an array of 'members'
-  // connected to the room (including us). Signaling server is ready.
-  room.on('members', members => {
-    console.log('MEMBERS', members);
-    // If we are the second user to connect to the room we will be creating the offer
-    const isOfferer = members.length === 2;
+
+  // Event members is invoked once upon connecting to a room and returns array with member list (including client).
+  room.on("members", members => {
+    console.log("MEMBERS", members);
+    const isOfferer = members.length === 2; // Returns boolean, where true means the client is the second person in the room.
     startWebRTC(isOfferer);
+  });
+
+  // member_join is invoked whenver someone joins the room.
+  room.on("member_join", member => {
+    let joinMessage = (member.clientData.name += " joined");
+    joinMessage += String.fromCharCode(13, 10); // line break
+    document.getElementById("notificationsBox").value += joinMessage;
+  });
+
+  room.on("member_leave", member => {
+    let leftMessage = (member.clientData.name += " left");
+    leftMessage += String.fromCharCode(13, 10);
+    document.getElementById("notificationsBox").value += leftMessage;
+    console.log(remoteVideo);
+  });
+
+  room.on("message", message => {
+    // Checks if message is containing meta-data or a chat message.
+    if (message.data.data) {
+      // If it's a chat message, post to chatBox element.
+      let chatBox = document.getElementById("chatBox");
+      chatBox.value += message.data.user += ": ";
+      chatBox.value += message.data.data;
+      chatBox.value += String.fromCharCode(13, 10);
+      chatBox.scrollTop = document.getElementById("chatBox").scrollHeight;
+    }
   });
 });
 
-// Send signaling data via Scaledrone
-function sendMessage(message) {
-  drone.publish({
-    room: roomName,
-    message
-  });
-}
+var keyPress = document.getElementById("chat_text_input");
+keyPress.addEventListener("keyup", function(event) {
+  if (event.keyCode === 13) document.getElementById("sendMsgBtn").click();
+});
 
 function startWebRTC(isOfferer) {
-  pc = new RTCPeerConnection(configuration);
+  // instances of RTCPeerConnection represent a connection between the local device and a remote peer.
+  peerConnection = new RTCPeerConnection(configuration);
 
-  // 'onicecandidate' notifies us whenever an ICE agent needs to deliver a
-  // message to the other peer through the signaling server
-  pc.onicecandidate = event => {
+  // This event occurs when the local ICE agent needs to deliver a message to the other peer through a signaling server.
+  peerConnection.onicecandidate = event => {
     if (event.candidate) {
-      sendMessage({'candidate': event.candidate});
+      SendMetaMessage({ candidate: event.candidate });
     }
   };
 
-  // If user is offerer let the 'negotiationneeded' event create the offer
+  // Handshake handling with respect to user being the first party in the room or not.
   if (isOfferer) {
-    pc.onnegotiationneeded = () => {
-      pc.createOffer().then(localDescCreated).catch(onError);
-    }
+    peerConnection.onnegotiationneeded = () => {
+      peerConnection
+        .createOffer()
+        .then(localDescCreated)
+        .catch(onError);
+    };
   }
 
-  // When a remote stream arrives display it in the #remoteVideo element
-  pc.ontrack = event => {
+  // Stream video from remote peer to video element on page.
+  peerConnection.ontrack = event => {
     const stream = event.streams[0];
     if (!remoteVideo.srcObject || remoteVideo.srcObject.id !== stream.id) {
       remoteVideo.srcObject = stream;
     }
   };
 
-  navigator.mediaDevices.getUserMedia({
-    audio: true,
-    video: true,
-  }).then(stream => {
-    // Display your local video in #localVideo element
-    localVideo.srcObject = stream;
-    // Add your stream to be sent to the conneting peer
-    stream.getTracks().forEach(track => pc.addTrack(track, stream));
-  }, onError);
+  // Capture video stream from local machine's webcam.
+  navigator.mediaDevices
+    .getUserMedia({
+      audio: true,
+      video: true
+    })
+    .then(stream => {
+      // Display your local video in #localVideo element
+      localVideo.srcObject = stream;
+      // Add your stream to be sent to the conneting peer
+      stream
+        .getTracks()
+        .forEach(track => peerConnection.addTrack(track, stream));
+    }, onError);
 
   // Listen to signaling data from Scaledrone
-  room.on('data', (message, client) => {
-    // Message was sent by us
+  room.on("data", (message, client) => {
     if (client.id === drone.clientId) {
+      // If message was sent by us, do nothing.
       return;
     }
 
     if (message.sdp) {
-      // This is called after receiving an offer or answer from another peer
-      pc.setRemoteDescription(new RTCSessionDescription(message.sdp), () => {
-        // When receiving an offer lets answer it
-        if (pc.remoteDescription.type === 'offer') {
-          pc.createAnswer().then(localDescCreated).catch(onError);
-        }
-      }, onError);
+      // Handshake handling
+      peerConnection.setRemoteDescription(
+        new RTCSessionDescription(message.sdp),
+        () => {
+          // When receiving an offer, answer it
+          if (peerConnection.remoteDescription.type === "offer") {
+            peerConnection
+              .createAnswer()
+              .then(localDescCreated)
+              .catch(onError);
+          }
+        },
+        onError
+      );
     } else if (message.candidate) {
       // Add the new ICE candidate to our connections remote description
-      pc.addIceCandidate(
-        new RTCIceCandidate(message.candidate), onSuccess, onError
+      peerConnection.addIceCandidate(
+        new RTCIceCandidate(message.candidate),
+        onSuccess,
+        onError
       );
     }
   });
 }
-
 function localDescCreated(desc) {
-  pc.setLocalDescription(
+  //Handshake handling
+  peerConnection.setLocalDescription(
     desc,
-    () => sendMessage({'sdp': pc.localDescription}),
+    () => SendMetaMessage({ sdp: peerConnection.localDescription }),
     onError
   );
+}
+
+function sendChatMessage() {
+  drone.publish({
+    room: `${roomName}`,
+    message: {
+      data: document.getElementById("chat_text_input").value,
+      user: userName
+    }
+  });
+  document.getElementById("chat_text_input").value = " "; //Clear chat input box
+}
+
+function onSuccess() {}
+function onError(error) {
+  console.error(error);
+  }
+
+// Send signaling data via Scaledrone
+function SendMetaMessage(message) {
+  drone.publish({
+    room: roomName,
+    message
+  });
 }
