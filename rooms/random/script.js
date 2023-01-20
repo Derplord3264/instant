a686bbb7a148294626bc4b041e4f0484683f0c38
Derplord3264 <96126289+Derplord3264@users.noreply.github.com>
2023-01-20 12:24:52 -0500

    Add files via upload

---
+++ rooms/random/script.js
@@ -0,0 +1,41 @@
+var drone = new ScaleDrone('ItjhW2SgVZ3jwl2x');
+
+drone.on('open', function (error) {
+  if (error) return console.error(error);
+  
+  var room = drone.subscribe('general-chat');
+  
+  room.on('open', function (error) {
+    if (error) return console.error(error);
+    console.log('Connected to room');
+  });
+
+  room.on('data', addMessageToScreen);
+});
+
+function onSubmitForm(event) {
+  var nameEl = document.querySelector('.input.name')
+    , contentEl = document.querySelector('.input.content');
+
+  if (nameEl.value && contentEl.value) {
+    sendMessageToScaleDrone(nameEl.value, contentEl.value);
+    contentEl.value = '';
+  }
+}
+
+function sendMessageToScaleDrone(name, content) {
+  drone.publish({
+    room: 'general-chat',
+    message: {
+      name: name,
+      content: content
+    }
+  });
+}
+
+function addMessageToScreen(message) {
+  var div = document.createElement('div');
+  div.innerHTML = '<b>' + message.name + '</b>: ' + message.content;
+  div.classList.add('message');
+  document.querySelector('.text-area').appendChild(div);
+}
---
+++ script.js
@@ -0,0 +1,5 @@
+function linkView() {
+  var code = document.querySelector('.input.content')
+  var location = './video/index.html'+ # + code
+  location.replace(location)
+}
